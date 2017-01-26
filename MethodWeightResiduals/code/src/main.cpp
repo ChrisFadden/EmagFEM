69540a7c966285aa2a41dc64d2a9b46d384701b8
ChrisFadden <chris_fadden@yahoo.com>
2017-01-25 21:19:49 -0500

    successfully meshed LaplaceFEM

--- MethodWeightResiduals/code/src/main.cpp
+++ MethodWeightResiduals/code/src/main.cpp
@@ -1,40 +1,48 @@
 #include <iostream>
 #include <vector>
 
-#include "viennacl/linalg/direct_solve.hpp"
-#include "viennacl/linalg/lu.hpp"
-#include "viennacl/matrix.hpp"
-#include "viennacl/vector.hpp"
+#include "viennagrid/config/default_configs.hpp"
+#include "viennagrid/forwards.hpp"
+#include "viennagrid/mesh/element_creation.hpp"
+#include "viennagrid/mesh/mesh.hpp"
+#include "viennagrid/point.hpp"
+
+typedef viennagrid::triangular_2d_mesh MeshType;
+typedef viennagrid::result_of::segmentation<MeshType>::type SegmentationType;
+typedef viennagrid::result_of::segment_handle<SegmentationType>::type
+    SegmentHandleType;
+
+typedef viennagrid::result_of::point<MeshType>::type PointType;
+typedef viennagrid::result_of::vertex_handle<MeshType>::type VertexHandleType;
+
+typedef viennagrid::result_of::cell<MeshType>::type CellType;
+typedef viennagrid::result_of::cell_range<SegmentHandleType>::type CellRange;
+typedef viennagrid::result_of::iterator<CellRange>::type CellIterator;
 
 int main() {
 
-  typedef float ScalarType;
+  // Construct mesh object
+  MeshType mesh;
 
-  viennacl::vector<ScalarType> b;
-  viennacl::vector<ScalarType> x;
-  viennacl::matrix<ScalarType> A(4, 4);
+  // Define points
+  VertexHandleType vh0 =
+      viennagrid::make_vertex(mesh, PointType(0.8, 1.8)); // id = 0
+  VertexHandleType vh1 =
+      viennagrid::make_vertex(mesh, PointType(1.4, 1.4)); // id = 1
+  VertexHandleType vh2 = viennagrid::make_vertex(mesh, PointType(2.1, 2.1));
+  VertexHandleType vh3 = viennagrid::make_vertex(mesh, PointType(1.2, 2.7));
 
-  std::vector<double> ab = {7.0, 1.0, 1.0, 3.0}; // entries of vector b
+  // Create triangles
+  viennagrid::make_triangle(mesh, vh0, vh1, vh3);
+  viennagrid::make_triangle(mesh, vh1, vh2, vh3);
 
-  std::vector<std::vector<double>> aA = {
-      {1.0, 2.0, 3.0, 0.0}, // entries of matrix A
-      {2.0, 1.0, -2.0, -3.0},
-      {-1.0, 1.0, 1.0, 0.0},
-      {0.0, 1.0, 1.0, -1.0}};
+  // Print out values
+  for (auto iter = viennagrid::elements<viennagrid::triangle_tag>(mesh).begin();
+       iter != viennagrid::elements<viennagrid::triangle_tag>(mesh).end();
+       iter++) {
 
-  viennacl::copy(ab, b);
-
-  for (int i = 0; i != 4; ++i) {
-    for (int j = 0; j != 4; ++j) {
-      A(i, j) = aA[i][j];
-    }
+    std::cout << *iter << std::endl;
   }
-
-  viennacl::linalg::lu_factorize(A);
-  x = b;
-  viennacl::linalg::lu_substitute(A, x);
-
-  std::cout << x << std::endl;
 
   return EXIT_SUCCESS;
 }
